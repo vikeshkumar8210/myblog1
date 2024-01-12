585028a6be2d492f3edd54011bdcb32fa5982039
vikesh <vikeshkr2ins@gmail.com>
2024-01-12 16:03:27 +0530

    Stream API

--- src/main/java/com/myblog/myblog1/TestClass1.java
+++ src/main/java/com/myblog/myblog1/TestClass1.java
@@ -1,15 +1,13 @@
 package com.myblog.myblog1;
 
-import jakarta.persistence.criteria.CriteriaBuilder;
-
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-import java.util.function.Consumer;
-import java.util.function.Predicate;
+import java.util.*;
 import java.util.stream.Collectors;
-import java.util.function.Function;
+
+
 public class TestClass1 {
+
     public static void main(String[] args) {
 
         //Examples of predicate
@@ -96,10 +94,26 @@ public class TestClass1 {
 
         // Consumer(forEach) example
 
-        List<String> names = Arrays.asList("mike", "adam", "sam");
-        Consumer<String> val = name-> System.out.println(name);
-        names.forEach(val);
+        //List<String> names = Arrays.asList("mike", "adam", "sam");
+        //Consumer<String> val = name-> System.out.println(name);
+        //names.forEach(val);
 
+        List<Login>logins=Arrays.asList(
+                new Login("mike","testing"),
+                new Login("stallin","testing"),
+                new Login("adam","testing")
+        );
 
+        List<LoginDto> dtos = logins.stream().map(login -> mapToDto(login)).collect(Collectors.toList());
+        System.out.println(dtos);
+
+    }
+
+    static LoginDto mapToDto(Login login){
+
+        LoginDto dto = new LoginDto();
+        dto.setUserName(login.getUserName());
+        dto.setPassword(login.getPassword());
+        return dto;
     }
 }
