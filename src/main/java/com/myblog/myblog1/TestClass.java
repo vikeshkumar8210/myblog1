6333e0586dbb36fcbd05c2075cf6d6f0a8dcce1d
vikesh <vikeshkr2ins@gmail.com>
2024-01-20 16:14:47 +0530

    Debugging the code in List

--- src/main/java/com/myblog/myblog1/TestClass.java
+++ src/main/java/com/myblog/myblog1/TestClass.java
@@ -2,17 +2,35 @@ package com.myblog.myblog1;
 
 import com.fasterxml.jackson.core.json.UTF8DataInputJsonParser;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class TestClass {
 
     public static void main(String[] args) {
-        TestClass testClass = new TestClass();
-        int val = testClass.test();
-        System.out.println(val);
-    }
-    public int test(){
-        int otp = SampleTest.test1();
+//        TestClass testClass = new TestClass();
+//        int val = testClass.test();
+//        System.out.println(val);
+//    }
+//    public int test(){
+//        int otp = SampleTest.test1();
+//
+//        return otp;
+        //Degugging in loop;
+//        for (int i=0; i<5; i++){
+//            System.out.println(i);
+//        }
 
-        return otp;
+        //Debugging in list;
+//        List<Integer> data = Arrays.asList(10, 20, 45, 78, 45, 12, 78, 20, 30, 10);
+//        List<Integer> val = data.stream().filter(i -> i % 2 == 0).collect(Collectors.toList());
+//        System.out.println(val);
+        
+        //Debugging in list with distinc/ unique value
+        List<Integer> data = Arrays.asList(10, 20, 45, 78, 45, 12, 78, 20, 30, 10);
+        List<Integer> val = data.stream().distinct().collect(Collectors.toList());
+        System.out.println(val);
     }
 }
 
